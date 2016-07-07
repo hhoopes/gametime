7ca53090bcfa741796e9514af9000c5e8291f4de
Josh Washke <jwashke@icloud.com>
2016-07-06 19:06:52 -0600

    Paddle test set up

---
+++ test/paddle.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+const paddle = require('../lib/paddle.js');
+
+describe('paddle', function() {
+  context('with default attributes', function() {
+    var paddle = new Paddle({});
+
+  });
+});
