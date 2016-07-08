356baada896a8d00bc0bf75b5323051cce1288a9
Josh Washke <jwashke@icloud.com>
2016-07-07 22:30:49 -0600

    Add constructor for paddle class

--- test/paddle.js
+++ test/paddle.js
@@ -3,7 +3,10 @@ const paddle = require('../lib/paddle.js');
 
 describe('paddle', function() {
   context('with default attributes', function() {
-    var paddle = new Paddle({});
+    var paddle = new Paddle({player: 1});
 
+    it('should assign a player that is passed in', function() {
+      expect.equal(paddle.player, 1);
+    });
   });
 });
